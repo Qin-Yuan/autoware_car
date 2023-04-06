661a8d9fd08ed7da6262cd1224a770787add209c
秦源赛 <1677168241@qq.com>
2023-04-06 19:20:01 +0800

    测试lidarslam

--- lidarslam_ros2/lidarslam/launch/lidarslam_tukuba.launch.py
+++ lidarslam_ros2/lidarslam/launch/lidarslam_tukuba.launch.py
@@ -2,7 +2,8 @@ import os
 
 import launch
 import launch_ros.actions
-
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
@@ -18,7 +19,8 @@ def generate_launch_description():
         package='scanmatcher',
         executable='scanmatcher_node',
         parameters=[main_param_dir],
-        remappings=[('/input_cloud','/points_raw')],
+        remappings=[('/input_cloud','/velodyne_V16/pointcloud2')],
+        # remappings=[('/input_cloud','/points_raw')],
         output='screen'
         )
 
@@ -34,14 +36,23 @@ def generate_launch_description():
         executable='static_transform_publisher',
         arguments=['0','0','0','0','0','0','1','base_link','velodyne']
         )
-
+    
+    rviz = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(get_package_share_directory('lidarslam'), 'rviz', 'mapping_tukuba.rviz')],
+        condition=IfCondition(LaunchConfiguration('rviz'))
+    )
 
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(
             'main_param_dir',
             default_value=main_param_dir,
             description='Full path to main parameter file to load'),
+        launch.actions.DeclareLaunchArgument('rviz', default_value='true',
+                            description='Open RViz.'),
         mapping,
         graphbasedslam,
-        tf,
+        # tf,
+        rviz,
             ])
